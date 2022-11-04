114c67f483d1f8533d014b4fac8f85e5bb593ee2
Glenn Winnink <gwinnink@student.codam.nl>
2022-11-04 16:30:13 +0100

    added error check (untested)

--- src/builtins/ft_pwd.c
+++ src/builtins/ft_pwd.c
@@ -6,10 +6,11 @@
 /*   By: gwinnink <gwinnink@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/04 14:39:44 by gwinnink          #+#    #+#             */
-/*   Updated: 2022/11/04 14:54:52 by gwinnink         ###   ########.fr       */
+/*   Updated: 2022/11/04 16:15:11 by gwinnink         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include <stdio.h>
 #include <unistd.h>
 #include "builtins.h"
 
@@ -18,6 +19,8 @@ int	ft_pwd(void)
 	char	*temp;
 
 	temp = getcwd(NULL, 0);
+	if (!temp)
+		exit (1);
 	printf("%s\n", temp);
 	free(temp);
 	g_code = 0;
