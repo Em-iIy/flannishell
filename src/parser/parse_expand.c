11209aef94f84599e47b79d0293a2a2f6e3a47c8
Glenn Winnink <gwinnink@student.codam.nl>
2022-11-04 12:44:58 +0100

    added env var stopping at non alnum chars

--- src/parser/parse_expand.c
+++ src/parser/parse_expand.c
@@ -6,12 +6,27 @@
 /*   By: gwinnink <gwinnink@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/14 16:54:32 by gwinnink          #+#    #+#             */
-/*   Updated: 2022/10/27 16:30:44 by gwinnink         ###   ########.fr       */
+/*   Updated: 2022/11/04 12:39:42 by gwinnink         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "environment.h"
 #include "libft.h"
+
+static char	*parser_expand_g_code(char *str)
+{
+	char	*ret;
+	char	*temp;
+
+	if (str && str[0] == '?')
+	{
+		temp = ft_itoa(g_code);
+		ret = ft_strjoin(temp, &str[1]);
+		free(temp);
+		return (ret);
+	}
+	return (NULL);
+}
 
 char	*parser_expand(t_env *env, char *str)
 {
@@ -20,14 +35,12 @@ char	*parser_expand(t_env *env, char *str)
 	int		i;
 
 	i = 0;
-	while (str[i] && str[i] != ' ')
+	temp = parser_expand_g_code(str);
+	if (temp)
+		return (temp);
+	while (str[i] && ft_isalnum(str[i]))
 		i++;
 	temp = ft_substr(str, 0, i);
-	if (!ft_strncmp(temp, "?", 2))
-	{
-		free(temp);
-		return (ft_itoa(g_code));
-	}
 	ret = ft_strjoin(get_env(env, temp), &str[i]);
 	if (!ret)
 		ret = ft_strdup(&str[i]);
