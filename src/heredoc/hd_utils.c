b09df326d5c59ed20b374049a371b3657bba2dcb
Franakin <fddp2008@hotmail.com>
2022-11-08 12:56:11 +0100

    working heredoc

--- src/heredoc/hd_utils.c
+++ src/heredoc/hd_utils.c
@@ -6,7 +6,7 @@
 /*   By: fpurdom <fpurdom@student.codam.nl>           +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2022/11/07 19:03:57 by fpurdom       #+#    #+#                 */
-/*   Updated: 2022/11/07 19:46:54 by fpurdom       ########   odam.nl         */
+/*   Updated: 2022/11/08 12:25:58 by fpurdom       ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,20 +17,43 @@
 #include <fcntl.h>
 #include <unistd.h>
 
-static void	write_to_file(t_file *file, char *line, int fd, t_env *env)
+static int	hd_expand(char *line, int fd, t_env *env)
 {
-	/*int	i;
+	int		i;
+	char	*key;
+	char	*env_var;
 
 	i = 0;
+	while (line[i] && ft_isalnum(line[i]))
+			i++;
+	key = ft_substr(line, 0, i);
+	env_var = get_env(env, key);
+	free(key);
+	if (env_var)
+		ft_putstr_fd(env_var, fd);
+	return (i);
+}
+
+static void	write_to_file(t_file *file, char *line, int fd, t_env *env)
+{
+	int		i;
+	char	*env_var;
+
+	i = 0;
+	if (file->hd_quotes)
+	{
+		ft_putstr_fd(line, fd);
+		ft_putchar_fd('\n', fd);
+		return ;
+	}
 	while (line[i])
 	{
 		if (line[i] == '$')
-		{
-			while (line[i] && ft_isalnum(line[i]))
-				i++;
-		}
-	}*/
-	ft_putstr_fd(line, fd);
+			i += hd_expand(&line[i + 1], fd, env);
+		else
+			ft_putchar_fd(line[i], fd);
+		i++;
+	}
 	ft_putchar_fd('\n', fd);
 }
 
