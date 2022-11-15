2de4884773aa4d192ea7c5f1b483b02c2ea7846d
Glenn Winnink <gwinnink@student.codam.nl>
2022-11-15 16:22:02 +0100

    made msg function more versatile

--- src/error_msg.c
+++ src/error_msg.c
@@ -1,12 +1,12 @@
 /* ************************************************************************** */
 /*                                                                            */
-/*                                                        ::::::::            */
-/*   error_msg.c                                        :+:    :+:            */
-/*                                                     +:+                    */
-/*   By: fpurdom <fpurdom@student.codam.nl>           +#+                     */
-/*                                                   +#+                      */
-/*   Created: 2022/11/14 19:17:41 by fpurdom       #+#    #+#                 */
-/*   Updated: 2022/11/14 19:38:32 by fpurdom       ########   odam.nl         */
+/*                                                        :::      ::::::::   */
+/*   error_msg.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: gwinnink <gwinnink@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/11/14 19:17:41 by fpurdom           #+#    #+#             */
+/*   Updated: 2022/11/15 14:38:10 by gwinnink         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,11 +15,17 @@
 int	display_error(char *name, char *msg1, char *msg2, char *msg3)
 {
 	ft_putstr_fd("minishell: ", 2);
-	ft_putstr_fd(name, 2);
-	ft_putstr_fd(": ", 2);
-	ft_putstr_fd(msg1, 2);
-	ft_putstr_fd(msg2, 2);
-	ft_putstr_fd(msg3, 2);
+	if (name)
+	{
+		ft_putstr_fd(name, 2);
+		ft_putstr_fd(": ", 2);
+	}
+	if (msg1)
+		ft_putstr_fd(msg1, 2);
+	if (msg2)
+		ft_putstr_fd(msg2, 2);
+	if (msg3)
+		ft_putstr_fd(msg3, 2);
 	ft_putchar_fd('\n', 2);
 	return (1);
 }
