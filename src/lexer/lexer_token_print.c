c01dd5963d97ea52473d64219d0433d8a9d9df64
Glenn Winnink <gwinnink@student.codam.nl>
2022-11-15 16:23:28 +0100

    norm

--- src/lexer/lexer_token_print.c
+++ src/lexer/lexer_token_print.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   lexer_token_print.c                                :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: gwinnink <gwinnink@student.codam.nl>       +#+  +:+       +#+        */
+/*   By: gwinnink <gwinnink@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/13 14:28:47 by gwinnink          #+#    #+#             */
-/*   Updated: 2022/10/18 20:48:26 by gwinnink         ###   ########.fr       */
+/*   Updated: 2022/11/15 14:51:13 by gwinnink         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,80 +14,80 @@
 #include "libft.h"
 #include "lexer.h"
 
-void	lexer_token_print(t_token *token)
-{
-	t_token	*temp;
-	int		i;
+// void	lexer_token_print(t_token *token)
+// {
+// 	t_token	*temp;
+// 	int		i;
 
-	i = 0;
-	if (!token)
-		return ;
-	temp = token;
-	printf("Tokens:\n--------------------------------------------------------------\nContent:\t");
-	while (temp)
-	{
-		printf("`%s` | ", temp->content);
-		temp = temp->next;
-	}
-	printf("\n");
-	temp = token;
-	printf("str_end:\t");
-	while (temp)
-	{
-		printf("`%d`", temp->str_end);
-		i = ft_strlen(temp->content) - 1;
-		while (i > 0)
-		{
-			printf(" ");
-			i--;
-		}
-		printf(" | ");
-		temp = temp->next;
-	}
-	printf("\n");
-	temp = token;
-	printf("iden:\t\t");
-	while (temp)
-	{
-		printf("`%d`", temp->iden);
-		i = ft_strlen(temp->content) - 1;
-		while (i > 0)
-		{
-			printf(" ");
-			i--;
-		}
-		printf(" | ");
-		temp = temp->next;
-	}
-	printf("\n");
-	temp = token;
-	printf("start:\t\t");
-	while (temp)
-	{
-		printf("`%d`", temp->start_pos);
-		i = ft_strlen(temp->content) - 1;
-		while (i > 0)
-		{
-			printf(" ");
-			i--;
-		}
-		printf(" | ");
-		temp = temp->next;
-	}
-	printf("\n");
-	temp = token;
-	printf("end:\t\t");
-	while (temp)
-	{
-		printf("`%d`", temp->end_pos);
-		i = ft_strlen(temp->content) - 1;
-		while (i > 0)
-		{
-			printf(" ");
-			i--;
-		}
-		printf(" | ");
-		temp = temp->next;
-	}
-	printf("\n");
-}
+// 	i = 0;
+// 	if (!token)
+// 		return ;
+// 	temp = token;
+// 	printf("Tokens:\n-----\nContent:\t");
+// 	while (temp)
+// 	{
+// 		printf("`%s` | ", temp->content);
+// 		temp = temp->next;
+// 	}
+// 	printf("\n");
+// 	temp = token;
+// 	printf("str_end:\t");
+// 	while (temp)
+// 	{
+// 		printf("`%d`", temp->str_end);
+// 		i = ft_strlen(temp->content) - 1;
+// 		while (i > 0)
+// 		{
+// 			printf(" ");
+// 			i--;
+// 		}
+// 		printf(" | ");
+// 		temp = temp->next;
+// 	}
+// 	printf("\n");
+// 	temp = token;
+// 	printf("iden:\t\t");
+// 	while (temp)
+// 	{
+// 		printf("`%d`", temp->iden);
+// 		i = ft_strlen(temp->content) - 1;
+// 		while (i > 0)
+// 		{
+// 			printf(" ");
+// 			i--;
+// 		}
+// 		printf(" | ");
+// 		temp = temp->next;
+// 	}
+// 	printf("\n");
+// 	temp = token;
+// 	printf("start:\t\t");
+// 	while (temp)
+// 	{
+// 		printf("`%d`", temp->start_pos);
+// 		i = ft_strlen(temp->content) - 1;
+// 		while (i > 0)
+// 		{
+// 			printf(" ");
+// 			i--;
+// 		}
+// 		printf(" | ");
+// 		temp = temp->next;
+// 	}
+// 	printf("\n");
+// 	temp = token;
+// 	printf("end:\t\t");
+// 	while (temp)
+// 	{
+// 		printf("`%d`", temp->end_pos);
+// 		i = ft_strlen(temp->content) - 1;
+// 		while (i > 0)
+// 		{
+// 			printf(" ");
+// 			i--;
+// 		}
+// 		printf(" | ");
+// 		temp = temp->next;
+// 	}
+// 	printf("\n");
+// }
--- src/main.c
+++ src/main.c
@@ -1,12 +1,12 @@
 /* ************************************************************************** */
 /*                                                                            */
-/*                                                        ::::::::            */
-/*   main.c                                             :+:    :+:            */
-/*                                                     +:+                    */
-/*   By: gwinnink <gwinnink@student.42.fr>            +#+                     */
-/*                                                   +#+                      */
-/*   Created: 2022/07/13 15:20:49 by gwinnink      #+#    #+#                 */
-/*   Updated: 2022/11/11 18:13:09 by fpurdom       ########   odam.nl         */
+/*                                                        :::      ::::::::   */
+/*   main.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: gwinnink <gwinnink@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/07/13 15:20:49 by gwinnink          #+#    #+#             */
+/*   Updated: 2022/11/15 16:05:55 by gwinnink         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -27,6 +27,45 @@
 
 int	g_code = 0;
 
+static int	lex_parse_exe(t_env *env, char *line)
+{
+	t_lexer		*lxr;
+	t_parser	*prsr;
+
+	lxr = lexer(line);
+	if (!lxr)
+		return (1);
+	prsr = parser(env, &lxr);
+	if (prsr->count > 0)
+		g_code = executor(prsr, &env);
+	if (g_code == 3)
+		perror("minishell");
+	free_lexer(&lxr);
+	free_parser(prsr);
+	return (0);
+}
+
+static void	minishell(t_env *env)
+{
+	char		*line;
+
+	while (1)
+	{
+		line = readline("minishell> ");
+		if (!line)
+		{
+			printf("exit\n");
+			exit(g_code);
+		}
+		if (line[0] == '\0')
+			continue ;
+		if (lex_parse_exe(env, line))
+			continue ;
+		add_history(line);
+		free(line);
+	}
+}
+
 int	main(int argc, char **argv, char **envp)
 {
 	char		*line;
@@ -42,27 +81,5 @@ int	main(int argc, char **argv, char **envp)
 	suppress_sig_output();
 	signal(SIGINT, sig_func_parent);
 	signal(SIGQUIT, SIG_IGN);
-	while (1)
-	{
-		line = readline("minishell> ");
-		if (!line)
-		{
-			printf("exit\n");
-			exit(g_code);
-		}
-		if (line[0] == '\0')
-			continue ;
-		add_history(line);
-		lxr = lexer(line);
-		if (!lxr)
-			continue ;
-		prsr = parser(env, &lxr);
-		if (prsr->count > 0)
-			g_code = executor(prsr, &env);
-		if (g_code == 3)
-			perror("minishell");
-		free_lexer(&lxr);
-		free_parser(prsr);
-		free(line);
-	}
+	minishell(env);
 }
