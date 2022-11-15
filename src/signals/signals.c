ff4528df05c7b81e3c600a02b725c16bcfd95241
Glenn Winnink <gwinnink@student.codam.nl>
2022-11-15 16:24:03 +0100

    fixed control characters + added explanation

--- src/signals/signals.c
+++ src/signals/signals.c
@@ -6,7 +6,7 @@
 /*   By: gwinnink <gwinnink@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/10 14:16:04 by fpurdom           #+#    #+#             */
-/*   Updated: 2022/11/15 14:31:30 by gwinnink         ###   ########.fr       */
+/*   Updated: 2022/11/15 16:09:02 by gwinnink         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,10 +21,45 @@
 
 extern int	g_code;
 
-
 /*
 Set terminal settings to default
 fixes SIGQUIT out of programs that change terminal settings (top)
+	// new_settings.c_lflag &= ~ECHOCTL;
+*/
+/* Setting definitions in termios.h
+These are the default settings bash starts off with!
+c_iflag:
+	- BRKINT          0x00000002      map BREAK to SIGINTR
+	- ICRNL           0x00000100      map CR to NL (ala CRMOD)
+	- IXON            0x00000200      enable output flow control
+	- IXANY           0x00000800      any char will restart after stop
+	- IMAXBEL         0x00002000      ring bell on input queue full
+	- IUTF8           0x00004000      maintain state for UTF-8 VERASE
+			total:	 <0x00006b02>
+
+c_oflag:
+	- OPOST           0x00000001      enable following output processing
+	- ONLCR           0x00000002      map NL to CR-NL (ala CRMOD)
+			total:	 <0x00000003>
+
+c_cflag:
+	- CSIZE           0x00000300      character size mask
+		- CS8             0x00000300      8 bits
+	- CREAD           0x00000800      enable receiver
+	- HUPCL           0x00004000      hang up on last close
+			total:	 <0x00004b00>
+
+c_lflag:
+	- ECHOKE          0x00000001      visual erase for line kill
+	- ECHOE           0x00000002      visually erase chars
+	- ECHOK           0x00000004      echo NL after line kill
+	- ECHO            0x00000008      enable echoing
+	- ISIG            0x00000080      enable signals INTR, QUIT, [D]SUSP
+	- ICANON          0x00000100      canonicalize input lines
+	- IEXTEN          0x00000400      enable DISCARD and LNEXT
+	- PENDIN          0x20000000      XXX retype pending input (state)
+			total:	 <0x2000058f>
+
 */
 void	suppress_sig_output(void)
 {
@@ -32,11 +67,10 @@ void	suppress_sig_output(void)
 
 	if (tcgetattr(0, &new_settings))
 		perror("minishell: tcsetattr");
-	// new_settings.c_lflag &= ~ECHOCTL;
 	new_settings.c_iflag = 0x6b02;
 	new_settings.c_oflag = 0x3;
 	new_settings.c_cflag = 0x4b00;
-	new_settings.c_lflag = 0x200005cf;
+	new_settings.c_lflag = 0x2000058f;
 	if (tcsetattr(0, 0, &new_settings))
 		perror("minishell: tcsetattr");
 }
