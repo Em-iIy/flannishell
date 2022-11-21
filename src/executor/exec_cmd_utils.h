2b953b93fb4f2169303f57ac657c85c8b07034eb
Franakin <fddp2008@hotmail.com>
2022-11-21 14:39:34 +0100

    norm

--- src/executor/exec_cmd_utils.h
+++ src/executor/exec_cmd_utils.h
@@ -6,14 +6,14 @@
 /*   By: fpurdom <fpurdom@student.codam.nl>           +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2022/11/21 12:59:01 by fpurdom       #+#    #+#                 */
-/*   Updated: 2022/11/21 13:00:13 by fpurdom       ########   odam.nl         */
+/*   Updated: 2022/11/21 14:39:20 by fpurdom       ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef EXEC_CMD_UTILS_H
 # define EXEC_CMD_UTILS_H
 
-#include "executor.h"
+# include "executor.h"
 
 int		has_path(char *command);
 void	dup_everything(t_cmd *command, t_pipe *pipes);
